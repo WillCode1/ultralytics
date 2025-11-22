18cb09aa5274a1ced7e56744c9cb1fc6e38a62c8
diwei <2623556566@qq.com>
2025-11-22 12:12:32 +0800

    asd

--- ultralytics/my_demo/model_pt_onnx_engine.py
+++ ultralytics/my_demo/model_pt_onnx_engine.py
@@ -8,9 +8,5 @@ model = YOLO("/home/jetson/code/ultralytics/ultralytics/data/export/yolo11n.pt")
 # model = YOLO("/home/jetson/code/ultralytics/ultralytics/data/export/yolo11n-obb.pt")
 
 # Export the model to TensorRT
-# model.export(format="engine", dynamic=True, imgsz=32)
-# model.export(format="engine", half=True, dynamic=True, imgsz=32)
-# model.export(format="engine", int8=True, dynamic=True, imgsz=32)
-# model.export(format="engine", int8=True, data="coco128.yaml", fraction=1.0, dynamic=True, imgsz=32)
-# model.export(format="engine", int8=True, data="coco128.yaml", fraction=1.0, dynamic=False, imgsz=32)
-model.export(format="engine", int8=True, device="dla:0", data="coco128.yaml", fraction=1.0, dynamic=False, imgsz=32)
+# model.export(format="engine", half=True, dynamic=False)
+model.export(format="engine", int8=True, device="dla:0", data="coco128.yaml", fraction=1.0)
